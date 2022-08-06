507a5924bc69033ff9e472472cd341465a8411bd
emuroku <m610310@gmail.com>
2022-08-06 19:41:30 +0900

    #1 actionmethod練習

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -3,43 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size:100px; text-align:right; color:#eee;
-    margin: -40px 0px -50px 0px;}
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt){
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
     //
-    public function index()
-    {
-        global $head, $style, $body, $end;
+    public function index(Request $request, Response $response){
 
-        $html = $head . tag('title', 'Hello/Index') . $style . $body
-            . tag('h1', 'Index') . tag('p', 'This is Index Page')
-            . '<a href="/hello/other"> go to Other Page</a>' . $end;
-        return $html;
-    }
+        $html = <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+        body { font-size: 16pt; color:#999;}
+        h1 {font-size:120pt; text-align:right; color:#fafafa;
+            margin:-50px 0 -120px 0;}
+        </style>
+        </head>
+        <body>
+            <h1>Hello</h1>
+            <h3>Request</h3>
+            <pre>{$request}</pre>
+            <h3>Response</h3>
+            <pre>{$response}</pre>
+        </body>
+        </html>
+        EOF;
 
-    public function other()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Other') . $style . $body
-        . tag('h1', 'Other') . tag('p', 'This is other Page') . $end;
-
-        return $html;
+        $response -> setContent($html);
+        return $response;
     }
 }
