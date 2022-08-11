534e5b0ee689f29d1cb7ef99679dc3bb86c93bd5
emuroku <m610310@gmail.com>
2022-08-11 10:28:36 +0900

    #11 消失ファイルを修復

--- laravelapp/config/app.php
+++ laravelapp/config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // カスタムしたプロバイダの追加
+        App\Providers\HelloServiceProvider::class,
+
     ],
 
     /*
---
+++ laravelapp/resources/views/hello/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>'message'={{$message}}</p>
+    <p>'view_message'={{$view_message}}</p>
+@endsection
+
+@section('footer')
+    copyright 2020 tuyano.
+@endsection
